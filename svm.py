0fdac69eff05a1b45f4e17ea7dc493e6d3dfae64
gotipamul <59207474+gotipamul@users.noreply.github.com>
2020-10-01 05:16:33 -0700

    Added Comment in SVM.py

--- svm.py
+++ svm.py
@@ -1,3 +1,6 @@
+#Support Vector Machine is a machine learning technique used in recent studies to forecast stock prices.
+# These are used as parameters to the SVM model. The model attempts to predict whether a stock price sometime in the future will be higher or lower than it is on a given day.
+
 from csv import reader
 from sklearn import svm
 import numpy as np
